b75f64ad6330c338cc566790faaa45313e24cd56
imJackym <imjackyrm@gmail.com>
2022-05-15 20:50:08 +0800

    bug fix App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { Layout, Space, Menu, Breadcrumb } from "antd";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react';
+import { Store } from './Store';
 
 import DogScreen from './screen/DogScreen';
 import DogAddScreen from './screen/DogAddScreen';
@@ -11,23 +13,41 @@ import SignInScreen from './screen/SignInScreen';
 import SignUpScreen from './screen/SignUpScreen';
 import NotFound from "./screen/NotFound";
 
-const { Header, Content, Footer } = Layout;
+const { Header, Content, } = Layout;
 
 export default function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+    window.location.href = '/signin';
+  };
+
   return (
     <Router>
       <Header>
-        <Menu theme="dark" mode="horizontal">
+        <Menu theme="dark" mode="horizontal" warnKey={1}>
           <Space>
-            <Link to="" className='disabled-link'>VT6003CEM Web API</Link>
-            <Link to="/dog">Dog</Link>
-            <Link to="/dog_add">Add Dog</Link>
-            <Link to="/dog/:id">Edit Dog</Link>
+            <Menu.Item warnKey="menu">
+              <Link to="" className='disabled-link'>VT6003CEM Web API</Link>
+            </Menu.Item>
+            <Link to="/">Dog</Link>
+            {/* <Link to="/dog_add">Add Dog</Link> */}
+            {/* <Link to="/dog/:id">Edit Dog</Link> */}
             <Link to="/user/:id">User Profile</Link>
             <Link to="/user_edit/:id">Edit Profile</Link>
-            <Link to="/SignIn">Sign In</Link>
-            <Link to="/SignUp">Sign Out</Link>
-            <Link to="/*">NotFound</Link>
+            {
+              userInfo ? (
+                <Link to="#signout" onClick={signoutHandler}>Sign Out</Link>
+              ) : (
+                <>
+                  <Link to="/SignIn">Sign In</Link>
+                  <Link to="/SignUp">Sign Up</Link>
+                </>
+              )
+            }
           </Space>
         </Menu>
       </Header>
@@ -37,7 +57,7 @@ export default function App() {
         </Breadcrumb>
         <Routes>
           <Route exact path="/" element={<DogScreen />} />
-          <Route path="/dog" element={<DogScreen />} />
+          <Route path="/" element={<DogScreen />} />
           <Route path="/dog_add" element={<DogAddScreen />} />
           <Route path="/dog/:id" element={<DogEditScreen />} />
           <Route path="/user/:id" element={<UserScreen />} />
@@ -47,6 +67,6 @@ export default function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Content>
-    </Router>
+    </Router >
   );
 }
